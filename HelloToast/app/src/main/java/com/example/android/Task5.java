c69e0b3a4e195742389b4e89e5c4a0dd91907738
rajeshbasnet01 <brajesh18@tbc.edu.np>
2022-03-13 13:40:46 +0545

    Completed lab exercise first and all coding challenges for first lab

---
+++ HelloToast/app/src/main/java/com/example/android/Task5.java
@@ -0,0 +1,14 @@
+package com.example.android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Task5 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task5);
+    }
+}
